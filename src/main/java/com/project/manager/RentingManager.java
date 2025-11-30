78b30773b71eb57d87044cf7f4f018fcf35f0af9
Fanantenana NyAina <hei.fanantenana@gmail.com>
2025-11-30 22:16:22 +0300

    feat: logger and exception handling in service and manager level

--- src/main/java/com/project/manager/RentingManager.java
+++ src/main/java/com/project/manager/RentingManager.java
@@ -4,10 +4,13 @@ import com.project.entity.mother.RentalItem;
 import com.project.entity.mother.Renter;
 import com.project.service.RentalItemService;
 import com.project.service.RenterService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RentingManager {
+    private static final Logger logger = LoggerFactory.getLogger(RentingManager.class);
     private final RenterService rentingService;
     private final RentalItemService rentalItemService;
 
@@ -17,7 +20,19 @@ public class RentingManager {
     }
 
     public Boolean rent(Renter renter, RentalItem item){
-        return rentingService.rentItem(renter, item);
+        logger.info("{} is trying to rent {}", renter.renterName(), item.getDetails());
+        try {
+            Boolean isSuccess = rentingService.rentItem(renter, item);
+            if (isSuccess) {
+                logger.info("Item : {} successfully rented by {}", item.getDetails(), renter.renterName());
+            } else {
+                logger.warn("Item : {} successfully rented by {}", item.getDetails(), renter.renterName());
+            }
+
+            return isSuccess;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String getRentalItemDetails(String rentalItemName){
--- src/main/java/com/project/service/RentalItemService.java
+++ src/main/java/com/project/service/RentalItemService.java
@@ -1,24 +1,40 @@
 package com.project.service;
 
+import com.project.exception.NotFoundException;
 import com.project.repository.RentalItemRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RentalItemService {
-    private RentalItemRepository rentalItemRepository;
+    private static final Logger logger = LoggerFactory.getLogger(RentalItemService.class);
+    private final RentalItemRepository rentalItemRepository;
 
     public RentalItemService(RentalItemRepository rentalItemRepository) {
         this.rentalItemRepository = rentalItemRepository;
     }
 
     public String getRentalItemDetails(String rentalItemName){
+        logger.info("getting rental item details {}", rentalItemName);
         var item = rentalItemRepository.findByName(rentalItemName);
+
+        if(item == null){
+            logger.warn("rental item not found {}", rentalItemName);
+            throw new NotFoundException("rental item not found " + rentalItemName );
+        }
 
         return item.getDetails();
     }
 
     public Double rentalItemPrice(String rentalItemName){
+        logger.info("getting rental item price {}", rentalItemName);
         var item =  rentalItemRepository.findByName(rentalItemName);
+
+        if(item == null){
+            logger.warn("rental item not found {}", rentalItemName);
+            throw new NotFoundException("rental item not found " + rentalItemName );
+        }
 
         return item.getPrice();
     }
